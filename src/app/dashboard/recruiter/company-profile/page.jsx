7b46c4979204382443e1ffa596931a8d75f88439
rashedulislam595 <rashedulislam956581@gmail.com>
2026-06-05 12:06:19 +0600

    added next link form dashboard sidebar

---
+++ src/app/dashboard/recruiter/company-profile/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CompanyProfilePage = () => {
+    return (
+        <div>
+            <h1 className='text-2xl font-bold mb-4'>Company Profile</h1>
+            <p>This is where you can view and edit your company profile information.</p>
+        </div>
+    );
+};
+
+export default CompanyProfilePage;
--- src/components/dashboard/DashboardSidebar.jsx
+++ src/components/dashboard/DashboardSidebar.jsx
@@ -1,26 +1,28 @@
-import { LayoutSideContent, Bell, Envelope, Gear, House, Magnifier, Person } from "@gravity-ui/icons";
+import { LayoutSideContent, Bell, Envelope, Gear, House, Plus, Person, Briefcase } from "@gravity-ui/icons";
 import { Button, Drawer } from "@heroui/react";
+import Link from "next/link";
 
 export default function DashboardSidebar() {
     const navItems = [
-        { icon: House, label: "Home" },
-        { icon: Magnifier, label: "Search" },
-        { icon: Bell, label: "Notifications" },
-        { icon: Envelope, label: "Messages" },
-        { icon: Person, label: "Profile" },
-        { icon: Gear, label: "Settings" },
+        { icon: House, href: '/dashboard/recruiter', label: "Home" },
+        { icon: Bell, href: '/dashboard/recruiter/jobs', label: "Jobs" },
+        { icon: Plus, href: '/dashboard/recruiter/jobs/new', label: "Add A New Job" },
+        { icon: Briefcase, href: '/dashboard/recruiter/company-profile', label: "Company Profile" },
+        { icon: Envelope,href:'/dashboard/recruiter', label: "Messages" },
+        { icon: Person, href: '/dashboard/recruiter', label: "Profile" },
+        { icon: Gear,href:'/dashboard/recruiter', label: "Settings" },
     ];
 
     const navLinks = <nav className="flex flex-col gap-1">
         {navItems.map((item) => (
-            <button
+            <Link
                 key={item.label}
                 className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
-                type="button"
+                href={item.href}
             >
                 <item.icon className="size-5 text-muted" />
                 {item.label}
-            </button>
+            </Link>
         ))}
     </nav>
 
